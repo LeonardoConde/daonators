5287c4b8a17485206944372f6311438e93fb7e66
DESKTOP-ST5G2N3\Yuji <victoryujisaito@gmail.com>
2022-10-11 19:58:58 -0300

    CU-2xx44wf - Navbar (Desktop)

---
+++ src/main/webapp/src/components/navbar/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="navbar">
+    <div class="container">
+      <div class="flex justify-between w-full">
+        <div class="flex items-center  px-2 w-full">
+          <!-- logo -->
+          <router-link to="/">
+            <div class="navbar__logo ">
+              <img src="@/assets/img/logo.png" class="h-20" />
+
+              <span class="m-2"> {{ $t('navbar.daonators') }} </span>
+            </div>
+          </router-link>
+          <!-- menu -->
+          <div class="navbar__menu">
+            <router-link
+              v-for="(item, i) in itens"
+              :key="i"
+              :to="item.path"
+              class="navbar__itens hover:text-white"
+            >
+              {{ item.name }}
+            </router-link>
+          </div>
+          <!-- walletConnect -->
+          <button class="navbar__walletConnect hover:text-black ">
+            <div class=" self-end">
+              {{ $t('navbar.walletConnect') }}
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+import {NavItem} from '@/model/NavItem'
+
+@Component
+export default class Navbar extends Vue {
+  itens: NavItem[] = (this.$t('navbar.itens') as unknown) as NavItem[]
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  @apply shadow-lg bg-primary;
+  &__logo {
+    @apply flex items-center py-2 px-5 font-bold text-4xl;
+  }
+  &__menu {
+    @apply hidden horiz w-full place-content-end;
+  }
+  &__itens {
+    @apply text-sm mr-10 capitalize text-xl text-white;
+  }
+  &__walletConnect {
+    @apply hidden flex items-end py-3 px-4 rounded-3xl border-2 border-black bg-yellow-400 text-lg bg-secudary;
+  }
+}
+</style>
