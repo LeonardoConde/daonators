5f8106bae9a926ff73e96fc275485aeb6324648d
LeonardoDizConde <leonardodizconde@gmail.com>
2022-10-29 15:54:54 -0300

    CU-36v6nqu - criar nome para as votações

--- src/main/database/update.sql
+++ src/main/database/update.sql
@@ -4,3 +4,9 @@
 INSERT INTO `daonators`.`campaing_type` (`idCampaingTypePk`, `name`, `active`) VALUES ('1', 'Donate', '1');
 INSERT INTO `daonators`.`campaing_type` (`idCampaingTypePk`, `name`, `active`) VALUES ('2', 'Add', '1');
 INSERT INTO `daonators`.`campaing_type` (`idCampaingTypePk`, `name`, `active`) VALUES ('3', 'Remove', '1');
+
+-- -----------------------------------------------------
+-- CU-36v6nqu - criar nome para as votações
+-- -----------------------------------------------------
+
+ALTER TABLE `daonators`.`campaing` ADD COLUMN `name` VARCHAR(45) NOT NULL AFTER `idCampaingTypePk`;
